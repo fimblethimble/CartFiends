d4a79344a72ed37d2c86a248b0e5bc5158fcffa7
andrescastillo36 <44930975+andrescastillo36@users.noreply.github.com>
2018-11-11 17:33:42 -0600

    Add files via upload

---
+++ evade.ino
@@ -0,0 +1,75 @@
+void evade() {
+if (distance1<30)
+{
+  while (distance1<30)
+ { //Turn right
+
+// this function will run the motors in both directions at a fixed speed
+  // turn on motor A
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enA, 255);
+  // turn on motor B
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enB, 255);  
+}
+//Move forward
+// this function will run the motors in one direction at a fixed speed
+  // turn on motor A
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enA, 255);
+  // turn on motor B
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enB, 255);
+  delay(1000); //NOTE: Delay = motor runtime
+  // now turn off motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+if (distance3<30)
+{
+  while (distance3<30)
+  {
+    //turn left
+     // this function will run the motors in both directions at a fixed speed
+  // turn on motor A
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enA, 255);
+  // turn on motor B
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enB, 255);
+}
+//Move forward
+// this function will run the motors in one direction at a fixed speed
+  // turn on motor A
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enA, 255);
+  // turn on motor B
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enB, 255);
+  delay(1000); //NOTE: Delay = motor runtime
+  // now turn off motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+
+}
+}
