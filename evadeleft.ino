6abcfe4cb8f31a4fcd6d8100c687bef64135c6ed
andrescastillo36 <44930975+andrescastillo36@users.noreply.github.com>
2018-11-11 17:34:07 -0600

    Add files via upload

---
+++ evadeleft.ino
@@ -0,0 +1,38 @@
+void evadeleft(){
+  if (distance1<30)
+  {//Turn Right
+    // this function will run the motors in both directions at a fixed speed
+  // turn on motor A
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enA, 255);
+  // turn on motor B
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enB, 255);
+  // run motors for delay(runtime)
+  delay(500);  //NOTE: Delay = motor runtime
+  //Move Forward
+   // this function will run the motors in one direction at a fixed speed
+  // turn on motor A
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enA, 255);
+  // turn on motor B
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enB, 255);
+  evade();
+  delay(2000); //NOTE: Delay = motor runtime
+  // now turn off motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  
+  }
+}
